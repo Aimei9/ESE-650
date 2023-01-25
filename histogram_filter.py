3f49515559fcb0dd66d798ec4eb9e61a355cf21f
Aimei <109504758+Aimei9@users.noreply.github.com>
2023-01-25 15:27:31 -0500

    Update histogram_filter.py

--- histogram_filter.py
+++ histogram_filter.py
@@ -18,3 +18,36 @@ class HistogramFilter(object):
         '''
 
         ### Your Algorithm goes Below.
+        
+        cmap = np.rot90(cmap,-1)
+        belief = np.rot90(belief,-1)
+        A = np.array(cmap).shape[0]
+        B = np.array(cmap).shape[1]
+        go = np.zeros((A,B))
+     
+        for i in range(A):
+            for j in range(B):
+                # Not out of bounds
+                if i+action[0]>=0 and i+action[0]<A and j+action[1]>=0 and j+action[1]<B: 
+                    go[i+action[0]][j+action[1]] += belief[i][j]*0.9
+                    go[i][j] += belief[i][j]*0.1
+                else:
+                    go[i][j] += belief[i][j]*1   # Condition remains unchanged
+        # Sensory data
+        res = 0
+        for i in range(A):
+            for j in range(B):
+                if cmap[i][j] == observation:  # The right data
+                    go[i][j] = 0.9*go[i][j]
+                    res += go[i][j]
+                else:                          # Incorrect figures
+                    go[i][j] = 0.1*go[i][j]
+                    res += go[i][j]
+        go /= res
+        temp = 0
+        for i in range(A):
+            for j in range(B):
+                if go[i][j] > temp:
+                    temp = go[i][j]
+                    state = [i,j]
+        return np.rot90(go, k=1), np.array(state)
